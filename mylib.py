5c363045a217e33248e5cee109da94a25bd8c9fb
PrafulAradhyamth <prafularadhyamath@gmail.com>
2023-06-29 11:27:59 +0200

    iterate

--- mylib.py
+++ mylib.py
@@ -9,4 +9,15 @@ def printer(text):
     return
 
 
+printer('praful')
 
+def voice2spectrogram():
+    y, sr = librosa.load()
+    S = np.abs(librosa.stft(y))
+
+
+    import matplotlib.pyplot as plt
+    fig, ax = plt.subplots()
+    img = librosa.display.specshow(librosa.amplitude_to_db(S,ref=np.max),y_axis='log', x_axis='time', ax=ax)
+    ax.set_title('Power spectrogram')
+    fig.colorbar(img, ax=ax, format="%+2.0f dB")
