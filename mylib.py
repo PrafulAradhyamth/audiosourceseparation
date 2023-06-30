77d8a94fd035621637a8c2955ed372c90cad20e9
PrafulAradhyamth <prafularadhyamath@gmail.com>
2023-06-30 10:46:04 +0200

    function

--- mylib.py
+++ mylib.py
@@ -1,12 +1,15 @@
 import numpy as np 
 import librosa
 import matplotlib.pyplot as plt
+from scipy import signal
+import audiofile
+import librosa
 
 
-
-def printer(text):
-    print(text)
-    return
+def printer(path):
+    y, sr = librosa.load(path)
+    print(path)
+    return y
 
 
 def voice2spectrogram(path):
@@ -16,5 +19,11 @@ def voice2spectrogram(path):
     img = librosa.display.specshow(librosa.amplitude_to_db(S,ref=np.max),y_axis='log', x_axis='time', ax=ax)
     ax.set_title('Power spectrogram')
     fig.colorbar(img, ax=ax, format="%+2.0f dB")
-    plt.imshow()
-    return
+    plt.show()
+    return img
+
+
+path = '/speech/db/mul/audio/music/tency2/part1/maneskin_i-wanna-be-your-slave_65162/multi/bass.wav'
+img = voice2spectrogram(path)
+
+plt.savefig('test.png')
